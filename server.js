b6316e3f0bad4ac4d32cf3f7ab8054ab798522dd
Geoffrey Sarpong <g_sarpong@mail.fhsu.edu>
2025-05-14 22:44:17 -0500

    fixing get

--- server.js
+++ server.js
@@ -1,33 +1,18 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import connectDB from './config/dbConn.js';
-import statesRoutes from './routes/states.js';
-
-dotenv.config();
 const app = express();
-connectDB();
-
-app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve HTML at root
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// API routes
-app.use('/states', statesRoutes);
-
-// 404 fallback
 app.all('*', (req, res) => {
   res.status(404);
-
   if (req.accepts('html')) {
     res.send('<h1>404 Not Found</h1>');
   } else if (req.accepts('json')) {
@@ -37,5 +22,5 @@ app.all('*', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
