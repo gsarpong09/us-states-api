27d268e43040c0ce3fbd75c4d5de5a5fea262bc1
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-05-16 20:42:18 -0500

    Update states.js

--- routes/states.js
+++ routes/states.js
@@ -17,30 +17,25 @@ const {
 
 const verifyState = require('../middleware/verifyState');
 
-// GET all states or filter by contig query
-router.route('/').get(getAllStates);
+// Route: /states/
+router.route('/')
+  .get(getAllStates);
 
-// GET one state
-router.route('/:state').get(verifyState, getState);
+// Route: /states/:state
+router.route('/:state')
+  .get(verifyState, getState);
 
-// GET / POST / PATCH / DELETE funfacts
-router
-  .route('/:state/funfact')
+// Route: /states/:state/funfact
+router.route('/:state/funfact')
   .get(verifyState, getRandomFunFact)
   .post(verifyState, postFunFacts)
   .patch(verifyState, patchFunFact)
   .delete(verifyState, deleteFunFact);
 
-// GET capital
+// Other /states/:state/X routes
 router.get('/:state/capital', verifyState, getCapital);
-
-// GET nickname
 router.get('/:state/nickname', verifyState, getNickname);
-
-// GET population
 router.get('/:state/population', verifyState, getPopulation);
-
-// GET admission date
 router.get('/:state/admission', verifyState, getAdmission);
 
 module.exports = router;
