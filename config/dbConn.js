ca5b0f6c71f15e420b6b274d001a3246d443d3a1
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-05-16 19:06:41 -0500

    Update dbConn.js

--- config/dbConn.js
+++ config/dbConn.js
@@ -4,11 +4,11 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err);
+    console.error('MongoDB connection error:', err);
   }
 };
 
