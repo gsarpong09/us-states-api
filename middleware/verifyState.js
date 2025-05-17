b09e97528ffcae01ad08cd13d3d227ec933d6247
gsarpong09 <g_sarpong@mail.fhsu.edu>
2025-05-16 20:31:40 -0500

    Update verifyState.js

--- middleware/verifyState.js
+++ middleware/verifyState.js
@@ -1,3 +1,4 @@
+// middleware/verifyState.js (CommonJS)
 const fs = require('fs');
 const path = require('path');
 
@@ -9,10 +10,12 @@ const stateCodes = statesData.map(state => state.code.toUpperCase());
 
 const verifyState = (req, res, next) => {
   const code = req.params.state?.toUpperCase();
+
   if (!stateCodes.includes(code)) {
     return res.status(400).json({ message: 'Invalid state abbreviation parameter' });
   }
-  req.code = code;
+
+  req.code = code; // Store normalized code
   next();
 };
 
